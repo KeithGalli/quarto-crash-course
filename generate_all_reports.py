6a707d4852a6c5144570938c07f2c0a07cd4b034
Keith Galli <k.galli603@gmail.com>
2024-11-19 09:28:54 -0500

    Finished parameterized report

--- generate_all_reports.py
+++ generate_all_reports.py
@@ -4,6 +4,8 @@ genres = ['action', 'adventure', 'drama', 'science fiction', 'thriller', 'comedy
           'crime', 'animation', 'fantasy', 'horror', 'family', 'mystery', 'western', 
           'history', 'music', 'war', 'romance', 'documentary']
 
+
 for genre in genres:
-    print(f"Generating report for {genre} movies")
-    os.system(f"quarto render parameterized-report.qmd -P genre:{genre} --output {genre}.html")
+    for i in range(5,25):
+        print(f"Generating report for {genre} movies with {i} movies")
+        os.system(f"quarto render parameterized-report.qmd -P genre:{genre} -P num_movies:{i} --output {genre}_{i}.pdf")
